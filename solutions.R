a57dee85ab314f46515d5e7d91defbf77a51e919
azwaien <19b2137@ubd.edu.bn>
2022-11-10 22:58:32 +0800

    Update solutions.R

--- solutions.R
+++ solutions.R
@@ -43,6 +43,7 @@ draw.chord.by.midpoint <- function(center.x, center.y, radius,
   return(draw.chord(points, side.length));
 }
 
+# METHOD A -----------------------------------------
 method.one <- function(center.x, center.y, radius){
   endpoint1.theta <- runif(1,0,2*pi);
   endpoint2.theta <- runif(1,0,2*pi);
@@ -55,7 +56,7 @@ method.one <- function(center.x, center.y, radius){
   return(c(midpoint.x,midpoint.y));
 }
 
-
+# METHOD B -----------------------------------------
 method.two <- function(center.x, center.y, radius){
   radius.theta = runif(1,0,2*pi);
   d.from.center = runif(1,0,radius);
@@ -64,3 +65,73 @@ method.two <- function(center.x, center.y, radius){
   
   return(c(midpoint.x,midpoint.y));
 }
+
+# METHOD C -----------------------------------------
+method.three <- function(center.x, center.y, radius){
+  midpoint.x = center.x - radius;
+  midpoint.y = center.y - radius;
+  while((midpoint.x-center.x) ^ 2 + (midpoint.y-center.y) ^ 2 > radius ^ 2){
+    dx.from.edge = runif(1,0,2*radius) ;
+    dy.from.edge = runif(1,0,2*radius) ;
+    midpoint.x = center.x - radius + dx.from.edge ;
+    midpoint.y = center.y - radius + dy.from.edge ;  
+  }
+  return(c(midpoint.x,midpoint.y));
+}
+
+# MAIN---------------------------------
+main <- function(){
+  n.trials <- 50;
+  xrange <- 49;
+  yrange <- 49;
+  center.x <- 25;
+  center.y <- 25;
+  radius <- 24;
+  hold.frames <- 25;
+  
+  generate.midpoints <- function(method){
+    midpoints <- vector(mode="list", length=n.trials);
+    
+    for(iteration in 1:n.trials){
+      midpoints[[iteration]] <- method(center.x, center.y, radius);
+    }
+    
+    return(midpoints);
+  }
+  
+  draw.frame <- function(midpoints, n.chords){
+    longer <- 0;
+    total <- 0;
+    plot(1:xrange, 1:yrange, type="n", xlab="", ylab="", main="Bertrand", asp=1);
+    draw.circle(center.x, center.y, radius);
+    for(chord.idx in 1:n.chords){
+      total <- total + 1;
+      midpoint <- midpoints[[chord.idx]];
+      is.longer <- draw.chord.by.midpoint(center.x, center.y, radius,
+                                          midpoint[1], midpoint[2]);
+      if(is.longer){
+        longer <- longer + 1;
+      }
+    }
+    string <- sprintf("Total: %d, Longer: %d, Probability: %f",
+                      total, longer, longer/total);
+    mtext(string);
+  }
+  
+  ani.options(interval=0.1);
+  ms <- c(method.one, method.two, method.three);
+  saveHTML({
+    for(method in ms){
+      midpoints <- generate.midpoints(method);
+      for(iteration in 1:n.trials){
+        draw.frame(midpoints, iteration);
+      }
+      for(i in 1:hold.frames){
+        draw.frame(midpoints, n.trials);
+      }
+    }
+  });
+}
+
+
+main()
